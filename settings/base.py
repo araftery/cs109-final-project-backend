eebf783ef2321721c541a066fc8a7944c81b7367
Andrew Raftery <andrewraftery@gmail.com>
2014-12-14 13:14:36 -0500

    fix cors headers for safari

--- settings/base.py
+++ settings/base.py
@@ -121,3 +121,14 @@ BUGSNAG = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_HEADERS = (
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'x-csrftoken',
+    'accept-encoding',
+    'accept-language'
+)
